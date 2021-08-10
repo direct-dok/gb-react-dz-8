89c11dbf13271d5c5e7d9b3e4454e69901257547
Viktor <you@example.com>
2021-08-10 14:30:04 +0500

    Start Project

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+
+import { Provider } from 'react-redux'
+import { persistor, store } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+
+import './index.css'
+
+ReactDOM.render(
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+      , 
+      document.getElementById('root')
+)
+
+
+
+
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './components/App'
+
+// import { Provider } from 'react-redux'
+// import { store } from './store/store'
+
+// import './index.css'
+
+// ReactDOM.render(
+//       <Provider store={store}>
+//         <App />
+//       </Provider>
+//       , 
+//       document.getElementById('root')
+// )
